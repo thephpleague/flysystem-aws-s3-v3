cda6f03d0fdad60ac457afd467ad4b668c73ea05
Frank de Jonge <info@frenky.net>
2014-11-13 16:16:34 +0100

    Initial implementations, is WIP

---
+++ spec/League/Flysystem/AwsS3v3/AdapterSpec.php
@@ -0,0 +1,276 @@
+<?php
+
+namespace spec\League\Flysystem\AwsS3v3;
+
+use Aws\Common\Result;
+use Aws\S3\Exception\S3Exception;
+use Aws\S3\S3Client;
+use GuzzleHttp\Command\CommandInterface;
+use GuzzleHttp\Stream\Stream;
+use League\Flysystem\Config;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class AdapterSpec extends ObjectBehavior
+{
+    private $client;
+    private $bucket;
+
+    function let(S3Client $client)
+    {
+        $this->client = $client;
+        $this->bucket = 'bucket';
+        $this->beConstructedWith($this->client, $this->bucket);
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('League\Flysystem\AwsS3v3\Adapter');
+        $this->shouldHaveType('League\Flysystem\AdapterInterface');
+    }
+
+    function it_should_write_files()
+    {
+        $this->make_it_write_using('write', 'contents');
+    }
+
+    function it_should_update_files()
+    {
+        $this->make_it_write_using('update', 'contents');
+    }
+
+    function it_should_write_files_streamed()
+    {
+        $stream = tmpfile();
+        $this->make_it_write_using('writeStream', $stream);
+        fclose($stream);
+    }
+
+    function it_should_update_files_streamed()
+    {
+        $stream = tmpfile();
+        $this->make_it_write_using('updateStream', $stream);
+        fclose($stream);
+    }
+
+    public function it_should_delete_files(CommandInterface $command)
+    {
+        $result = new Result(['DeleteMarker' => true]);
+        $this->client->getCommand('deleteObject', [
+            'Bucket' => $this->bucket,
+            'Key' => $key = 'key.txt',
+        ])->willReturn($command);
+
+        $this->client->execute($command)->willReturn($result);
+        $this->delete($key)->shouldBe(true);
+    }
+
+    public function it_should_read_a_file(CommandInterface $command)
+    {
+        $this->make_it_read_a_file($command, 'read', 'contents');
+    }
+
+    public function it_should_read_a_file_stream(CommandInterface $command)
+    {
+        $resource = tmpfile();
+        $this->make_it_read_a_file($command, 'readStream', $resource);
+        fclose($resource);
+    }
+
+    public function it_should_return_when_trying_to_read_an_non_existing_file(
+        CommandInterface $command
+    ) {
+        $this->client->getCommand('getObject', [
+            'Bucket' => $this->bucket,
+            'Key' => $key = 'key.txt',
+        ])->willReturn($command);
+
+        $this->client->execute($command)->willThrow('GuzzleHttp\Exception\RequestException');
+
+        $this->read($key)->shouldBe(false);
+    }
+
+    public function it_should_retrieve_all_file_metadata(CommandInterface $command)
+    {
+        $this->make_it_retrieve_file_metadata('getMetadata', $command);
+    }
+
+    public function it_should_retrieve_the_timestamp_of_a_file(CommandInterface $command)
+    {
+        $this->make_it_retrieve_file_metadata('getTimestamp', $command);
+    }
+
+    public function it_should_retrieve_the_mimetype_of_a_file(CommandInterface $command)
+    {
+        $this->make_it_retrieve_file_metadata('getMimetype', $command);
+    }
+
+    public function it_should_retrieve_the_size_of_a_file(CommandInterface $command)
+    {
+        $this->make_it_retrieve_file_metadata('getSize', $command);
+    }
+
+    public function it_should_retrieve_the_metadata_to_check_if_an_object_exists(CommandInterface $command)
+    {
+        $this->make_it_retrieve_file_metadata('has', $command);
+    }
+
+    public function it_should_copy_files(CommandInterface $command)
+    {
+        $this->client->getCommand('copyObject', [
+            'Bucket' => $this->bucket,
+            'Key' => $key = 'key.txt',
+            'CopySource' => $this->bucket . '/' . ($sourceKey = 'newkey.txt'),
+        ])->willReturn($command);
+
+        $this->client->execute($command)->shouldBeCalled();
+        $this->copy($sourceKey, $key)->shouldBe(true);
+    }
+
+    public function it_should_return_false_when_copy_fails(CommandInterface $command)
+    {
+        $this->client->getCommand('copyObject', [
+            'Bucket' => $this->bucket,
+            'Key' => $key = 'key.txt',
+            'CopySource' => $this->bucket . '/' . ($sourceKey = 'newkey.txt'),
+        ])->willReturn($command);
+
+        $this->client->execute($command)->willThrow('Aws\S3\Exception\S3Exception');
+        $this->copy($sourceKey, $key)->shouldBe(false);
+    }
+
+    public function it_should_create_directories()
+    {
+        $config = new Config;
+        $path = 'dir/name';
+        $body = '';
+        $this->client->upload(
+            $this->bucket,
+            $path . '/',
+            $body,
+            Argument::type('array')
+        )->shouldBeCalled();
+
+        $this->createDir($path, $config)->shouldBeArray();
+    }
+    public function it_should_return_false_during_rename_when_copy_fails(CommandInterface $command)
+    {
+        $this->client->getCommand('copyObject', [
+            'Bucket' => $this->bucket,
+            'Key' => $key = 'key.txt',
+            'CopySource' => $this->bucket . '/' . ($sourceKey = 'newkey.txt'),
+        ])->willReturn($command);
+
+        $this->client->execute($command)->willThrow('Aws\S3\Exception\S3Exception');
+        $this->rename($sourceKey, $key)->shouldBe(false);
+    }
+
+    public function it_should_copy_and_delete_during_renames(CommandInterface $copyCommand, CommandInterface $deleteCommand)
+    {
+        $this->client->getCommand('copyObject', [
+            'Bucket' => $this->bucket,
+            'Key' => $key = 'key.txt',
+            'CopySource' => $this->bucket . '/' . ($sourceKey = 'newkey.txt'),
+        ])->willReturn($copyCommand);
+
+        $this->client->execute($copyCommand)->shouldBeCalled();
+
+        $deleteResult = new Result(['DeleteMarker' => true]);
+
+        $this->client->getCommand('deleteObject', [
+            'Bucket' => $this->bucket,
+            'Key' => $sourceKey,
+        ])->willReturn($deleteCommand);
+
+        $this->client->execute($deleteCommand)->willReturn($deleteResult);
+
+        $this->rename($sourceKey, $key)->shouldBe(true);
+    }
+
+    public function it_should_list_contents(CommandInterface $command)
+    {
+        $prefix = 'prefix';
+
+        $this->client->getCommand('listObjects', [
+            'Bucket' => $this->bucket,
+            'Prefix' => $prefix . '/',
+        ])->willReturn($command);
+
+        $result = new Result([
+            'Contents' => [
+                ['Key' => 'prefix/filekey.txt'],
+                ['Key' => 'prefix/dirname/'],
+            ]
+        ]);
+
+        $this->client->execute($command)->willReturn($result);
+
+        $this->listContents($prefix)->shouldHaveCount(2);
+    }
+
+    public function it_should_delete_directories()
+    {
+
+    }
+
+    public function it_should_get_the_visibility_of_a_file()
+    {
+
+    }
+
+    public function it_should_set_the_visibility_of_a_file()
+    {
+
+    }
+
+    protected function make_it_retrieve_file_metadata($method, CommandInterface $command)
+    {
+        $timestamp = time();
+
+        $result = new Result([
+            'Key' => $key = 'key.txt',
+            'LastModified' => date('Y-m-d H:i:s', $timestamp),
+            'ContentType' => 'plain/text',
+        ]);
+
+        $this->client->getCommand('headObject', [
+            'Bucket' => $this->bucket,
+            'Key' => $key,
+        ])->willReturn($command);
+
+        $this->client->execute($command)->willReturn($result);
+        $this->{$method}($key)->shouldBeArray();
+    }
+
+    protected function make_it_read_a_file(CommandInterface $command, $method, $contents)
+    {
+        $key = 'key.txt';
+        $stream = Stream::factory($contents);
+        $result = new Result([
+            'Key' => $key,
+            'LastModified' => $date = date('Y-m-d h:i:s'),
+            'Body' => $stream,
+        ]);
+        $this->client->getCommand('getObject', [
+            'Bucket' => $this->bucket,
+            'Key' => $key,
+        ])->willReturn($command);
+
+        $this->client->execute($command)->willReturn($result);
+        $this->{$method}($key)->shouldBeArray();
+    }
+
+    protected function make_it_write_using($method, $body)
+    {
+        $config = new Config;
+        $path = 'key.txt';
+        $this->client->upload(
+            $this->bucket,
+            $path,
+            $body,
+            Argument::type('array')
+        )->shouldBeCalled();
+
+        $this->{$method}($path, $body, $config)->shouldBeArray();
+    }
+}
---
+++ src/Adapter.php
@@ -0,0 +1,459 @@
+<?php
+
+namespace League\Flysystem\AwsS3v3;
+
+use Aws\S3\Exception\S3Exception;
+use Aws\S3\S3Client;
+use Aws\Common\Result;
+use Aws\S3\Exception\ObjectNotInActiveTierError;
+use GuzzleHttp\Exception\RequestException;
+use League\Flysystem\Adapter\AbstractAdapter;
+use League\Flysystem\AdapterInterface;
+use League\Flysystem\Config;
+use League\Flysystem\Util;
+
+class Adapter extends AbstractAdapter
+{
+    /**
+     * @var  array  $resultMap
+     */
+    protected static $resultMap = array(
+        'Body'          => 'contents',
+        'ContentLength' => 'size',
+        'ContentType'   => 'mimetype',
+        'Size'          => 'size',
+    );
+
+    /**
+     * @var  array  $metaOptions
+     */
+    protected static $metaOptions = array(
+        'CacheControl',
+        'Expires',
+        'StorageClass',
+        'ServerSideEncryption',
+        'Metadata',
+        'ACL',
+        'ContentType'
+    );
+
+    /**
+     * @var S3Client
+     */
+    protected $s3Client;
+
+    /**
+     * @var string
+     */
+    protected $bucket;
+
+
+    /**
+     * Constructor
+     *
+     * @param S3Client $client
+     * @param          $bucket
+     * @param string   $prefix
+     */
+    public function __construct(S3Client $client, $bucket, $prefix = '/')
+    {
+        $this->s3Client = $client;
+        $this->bucket = $bucket;
+        $this->setPathPrefix($prefix);
+    }
+
+    /**
+     * Write a new file
+     *
+     * @param   string $path
+     * @param   string $contents
+     * @param   Config $config Config object
+     * @return  false|array  false on failure file meta data on success
+     */
+    public function write($path, $contents, Config $config)
+    {
+        return $this->upload($path, $contents, $config);
+    }
+
+    /**
+     * Update a file
+     *
+     * @param   string $path
+     * @param   string $contents
+     * @param   Config $config Config object
+     * @return  false|array  false on failure file meta data on success
+     */
+    public function update($path, $contents, Config $config)
+    {
+        return $this->upload($path, $contents, $config);
+    }
+
+    /**
+     * Rename a file
+     *
+     * @param   string $path
+     * @param   string $newpath
+     * @return  boolean
+     */
+    public function rename($path, $newpath)
+    {
+        if (! $this->copy($path, $newpath)) {
+            return false;
+        }
+
+        return $this->delete($path);
+    }
+
+    /**
+     * Delete a file
+     *
+     * @param   string $path
+     * @return  boolean
+     */
+    public function delete($path)
+    {
+        $location = $this->applyPathPrefix($path);
+
+        $command = $this->s3Client->getCommand('deleteObject', [
+            'Bucket' => $this->bucket,
+            'Key' => $location,
+        ]);
+
+        /** @var Result $response */
+        $response = $this->s3Client->execute($command);
+
+        return $response->get('DeleteMarker');
+    }
+
+    /**
+     * Delete a directory
+     *
+     * @param   string $dirname
+     * @return  boolean
+     */
+    public function deleteDir($dirname)
+    {
+        // TODO: implement deleteDir
+    }
+
+    /**
+     * Create a directory
+     *
+     * @param   string $dirname directory name
+     * @param   Config $config
+     *
+     * @return  bool|array
+     */
+    public function createDir($dirname, Config $config)
+    {
+        return $this->upload($dirname . '/', '', $config);
+    }
+
+    /**
+     * Check whether a file exists
+     *
+     * @param   string $path
+     * @return  bool
+     */
+    public function has($path)
+    {
+        return $this->getMetadata($path);
+    }
+
+    /**
+     * Read a file
+     *
+     * @param   string $path
+     * @return  false|array
+     */
+    public function read($path)
+    {
+        $response = $this->readObject($path);
+
+        if ($response !== false) {
+            $response['contents'] = $response['contents']->getContents();
+        }
+
+        return $response;
+    }
+
+    /**
+     * List contents of a directory
+     *
+     * @param   string $directory
+     * @param   bool   $recursive
+     * @return  array
+     */
+    public function listContents($directory = '', $recursive = false)
+    {
+        $prefix = $this->applyPathPrefix(rtrim($directory, '/') . '/');
+        $command = $this->s3Client->getCommand('listObjects', [
+            'Bucket' => $this->bucket,
+            'Prefix' => $prefix
+        ]);
+
+        /** @var Result $result */
+        $result = $this->s3Client->execute($command);
+
+        return array_map([$this, 'normalizeResponse'], $result->get('Contents'));
+    }
+
+    /**
+     * Get all the meta data of a file or directory
+     *
+     * @param   string $path
+     * @return  false|array
+     */
+    public function getMetadata($path)
+    {
+        $command = $this->s3Client->getCommand('headObject', [
+            'Bucket' => $this->bucket,
+            'Key' => $this->applyPathPrefix($path),
+        ]);
+
+        /** @var Result $response */
+        $response = $this->s3Client->execute($command);
+
+        return $this->normalizeResponse($response->toArray(), $path);
+    }
+
+    /**
+     * Get all the meta data of a file or directory
+     *
+     * @param   string $path
+     * @return  false|array
+     */
+    public function getSize($path)
+    {
+        return $this->getMetadata($path);
+    }
+
+    /**
+     * Get the mimetype of a file
+     *
+     * @param   string $path
+     * @return  false|array
+     */
+    public function getMimetype($path)
+    {
+        return $this->getMetadata($path);
+    }
+
+    /**
+     * Get the timestamp of a file
+     *
+     * @param   string $path
+     * @return  false|array
+     */
+    public function getTimestamp($path)
+    {
+        return $this->getMetadata($path);
+    }
+
+    /**
+     * Write a new file using a stream
+     *
+     * @param   string   $path
+     * @param   resource $resource
+     * @param   Config   $config Config object
+     * @return  array|false  false on failure file meta data on success
+     */
+    public function writeStream($path, $resource, Config $config)
+    {
+        return $this->upload($path, $resource, $config);
+    }
+
+    /**
+     * Update a file using a stream
+     *
+     * @param   string   $path
+     * @param   resource $resource
+     * @param   Config   $config Config object
+     * @return  array|false  false on failure file meta data on success
+     */
+    public function updateStream($path, $resource, Config $config)
+    {
+        return $this->upload($path, $resource, $config);
+    }
+
+    /**
+     * Copy a file
+     *
+     * @param   string $path
+     * @param   string $newpath
+     * @return  boolean
+     */
+    public function copy($path, $newpath)
+    {
+        $command = $this->s3Client->getCommand('copyObject', [
+            'Bucket' => $this->bucket,
+            'Key' => $this->applyPathPrefix($newpath),
+            'CopySource' => $this->bucket . '/' . $this->applyPathPrefix($path),
+            // 'ACL' => $this->getObjectAcl($path), // TODO: get the objects ACL when copying.
+        ]);
+
+        try {
+            $this->s3Client->execute($command);
+        } catch (S3Exception $e) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Read a file as a stream
+     *
+     * @param   string $path
+     * @return  array|false
+     */
+    public function readStream($path)
+    {
+        $response = $this->readObject($path);
+
+        if ($response !== false) {
+            $response['stream'] = $response['contents']->detach();
+            unset($response['contents']);
+        }
+
+        return $response;
+    }
+
+    /**
+     * Read an object and normalize the response.
+     *
+     * @param $path
+     * @return array|bool
+     */
+    protected function readObject($path)
+    {
+        $command = $this->s3Client->getCommand('getObject', [
+            'Bucket' => $this->bucket,
+            'Key'    => $this->applyPathPrefix($path),
+        ]);
+
+        try {
+            /** @var Result $response */
+            $response = $this->s3Client->execute($command);
+        } catch (RequestException $e) {
+            return false;
+        }
+
+        return $this->normalizeResponse($response->toArray(), $path);
+    }
+
+    /**
+     * Set the visibility for a file
+     *
+     * @param   string $path
+     * @param   string $visibility
+     * @return  array|false   file meta data
+     */
+    public function setVisibility($path, $visibility)
+    {
+        // TODO: Implement setVisibility() method.
+    }
+
+    /**
+     * Get the visibility of a file
+     *
+     * @param   string $path
+     * @return  array|false
+     */
+    public function getVisibility($path)
+    {
+        // TODO: Implement getVisibility() method.
+    }
+
+    /**
+     * Upload an object.
+     *
+     * @param        $path
+     * @param        $body
+     * @param Config $config
+     * @return array
+     */
+    protected function upload($path, $body, Config $config)
+    {
+        $key = $this->applyPathPrefix($path);
+        $options = $this->getOptionsFromConfig($config);
+
+        if (! isset($options['ContentType']) && is_string($body)) {
+            $options['ContentType'] = Util::guessMimeType($path, $body);
+        }
+
+        if (! isset($options['ContentLength'])) {
+            $options['ContentLength'] = is_string($body) ? Util::contentSize($body) : Util::getStreamSize($body);
+        }
+
+        $this->s3Client->upload($this->bucket, $key, $body, $options);
+
+        return $this->normalizeResponse($options, $key);
+    }
+
+    /**
+     * Get options from the config.
+     *
+     * @param Config $config
+     * @return array
+     */
+    protected function getOptionsFromConfig(Config $config)
+    {
+        $options = [];
+
+        if ($visibility = $config->get('visibility')) {
+            // For local reference
+            $options['visibility'] = $visibility;
+            // For external reference
+            $options['ACL'] = $visibility === AdapterInterface::VISIBILITY_PUBLIC ? 'public-read' : 'private';
+        }
+
+        if ($mimetype = $config->get('mimetype')) {
+            // For local reference
+            $options['mimetype'] = $mimetype;
+            // For external reference
+            $options['ContentType'] = $mimetype;
+        }
+
+        foreach (static::$metaOptions as $option) {
+            if (! $config->has($option)) {
+                continue;
+            }
+            $options[$option] = $config->get($option);
+        }
+
+        return $options;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function applyPathPrefix($prefix)
+    {
+        return ltrim(parent::applyPathPrefix($prefix), '/');
+    }
+
+    /**
+     * Normalize the object result array.
+     *
+     * @param array $response
+     * @return array
+     */
+    protected function normalizeResponse(array $response, $path = null)
+    {
+        $result = array('path' => $path ?: $this->removePathPrefix($response['Key']));
+
+        if (isset($response['LastModified'])) {
+            $result['timestamp'] = strtotime($response['LastModified']);
+        }
+
+        if (substr($result['path'], -1) === '/') {
+            $result['type'] = 'dir';
+            $result['path'] = rtrim($result['path'], '/');
+
+            return $result;
+        }
+
+        return array_merge($result, Util::map($response, static::$resultMap), array('type' => 'file'));
+    }
+}
