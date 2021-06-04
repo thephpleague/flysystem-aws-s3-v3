82997fd1547c13efbc24aad6c47e209321abba73
Marvin Linthorst <4425296+marvinlinthorst@users.noreply.github.com>
2021-06-04 10:20:17 +0200

    Update AwsS3Adapter.php

--- src/AwsS3Adapter.php
+++ src/AwsS3Adapter.php
@@ -601,14 +601,14 @@ class AwsS3Adapter extends AbstractAdapter implements CanOverwriteFiles
                 unset($options['ContentLength']);
             }
             
-            if (! isset($options['Content-MD5'])) {
+            if (! isset($options['ContentMD5'])) {
                 if (is_resource($body)) {
                     $ctx = hash_init('MD5');
                     hash_update_stream($ctx, $body);
                     rewind($body);
-                    $options['Content-MD5'] = base64_encode(hash_final($ctx, true));
+                    $options['ContentMD5'] = base64_encode(hash_final($ctx, true));
                 } else {
-                    $options['Content-MD5'] = base64_encode(md5($body, true));
+                    $options['ContentMD5'] = base64_encode(md5($body, true));
                 }
             }
         }
